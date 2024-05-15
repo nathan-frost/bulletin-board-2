3a99a3d3ec7e426d2fe04558028879ab7cd325b0
nathan-frost <164823855+nathan-frost@users.noreply.github.com>
2024-05-15 23:25:05 +0000

    set up devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.sign_out_via = :get
   # Allow server to be hosted on any URL
   config.hosts.clear
   # Allow better_errors to work in online IDE
